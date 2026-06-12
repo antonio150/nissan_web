bf43208334182fd3419019c8b520553a7588aea5
antoniorollandee <antoniorollande@gmail.com>
2026-06-12 18:57:09 +0300

    ajout modal carsdd

--- app/section/offerDetails/page.tsx
+++ app/section/offerDetails/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaTimes } from "react-icons/fa";
 
 export default function OfferDetails({ nom }: { nom: string }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -43,7 +43,7 @@ export default function OfferDetails({ nom }: { nom: string }) {
                 onClick={() => setIsOpen(false)}
                 className="rounded-full bg-white/5 px-3 py-2 text-sm text-gray-900 transition hover:bg-white/10"
               >
-                x
+                <FaTimes />
               </button>
             </div>
 
